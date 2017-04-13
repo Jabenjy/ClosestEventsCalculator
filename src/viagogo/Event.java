de2175a41d8ba5c8f7d50f4d86432e42d2156ea3
Jabenjy <jabenjy@gmail.com>
2017-04-13 10:30:37 +0100

    completed task

--- src/viagogo/Event.java
+++ src/viagogo/Event.java
@@ -2,22 +2,44 @@ package viagogo;
 
 import java.awt.Point;
 import java.awt.geom.Point2D;
-import java.util.Random;
+import java.util.Arrays;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Event {
 	private int eventID;
-	private double price;
+	private double[] price = new double[5];
 	private Point location = new Point();
 	
+	/**
+	 * Create a new event with a given ID and location. 
+	 * Prices are generated automatically.
+	 * @param id
+	 * @param loc
+	 */
 	public Event(int id, Point loc){
 			eventID = id;
 			location = loc;
-			price = ThreadLocalRandom.current().nextDouble(10, 100);
-		}
+			for (int i = 0; i < price.length; i++){
+				price[i] = ThreadLocalRandom.current().nextDouble(10, 100);
+			}
+			Arrays.sort(price);
+	}
 	
-	public int getDistance(Point userLoc){
-		int dist = (int) Point2D.distance(userLoc.getX(), userLoc.getY(), 
+	/**
+	 * Returns a formatted string containing the details of the event.
+	 * @return
+	 */
+	public String getDetails(){
+		return "Event " + eventID + " - $" + String.format("%.2f", price[0]) + ", Location " + (int)location.getX() + "," + (int)location.getY();
+	}
+	
+	/**
+	 * Returns the distance of this event from a given location.
+	 * @param userLoc
+	 * @return
+	 */
+	public int getDistance(Point uPos){
+		int dist = (int) Point2D.distance(uPos.getX(), uPos.getY(), 
 				location.getX(), location.getY());
 		return dist;
 	}
--- src/viagogo/Main.java
+++ src/viagogo/Main.java
@@ -2,39 +2,85 @@ package viagogo;
 
 //import java.awt.List;
 import java.awt.Point;
-import java.awt.geom.Point2D;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Main {
 	public static void main(String[] args){
 		Point uPos = new Point();
-		
-		System.out.println("Please enter a coordinate: ");
+		List<Event> events = new ArrayList<Event>();
+		List<Event> closestEvents = new ArrayList<Event>();
 		ArrayList<Point> list = new ArrayList<Point>(genLoc());
-		Scanner uIn = new Scanner(System.in);
-		String coordinates = uIn.nextLine();
-		System.out.print(coordinates);
+		//Create a list of events
+		for(int i = 0; i < list.size(); i++){
+			Event instance = new Event(i, list.get(i));
+			events.add(instance);
+		}
+		
+		uPos = getUserPos();
+		
+		closestEvents = sortEvents(events, uPos).stream().limit(5).collect(Collectors.toList());
+		
+		showEvents(closestEvents, uPos);
+	}
+	/**
+	 * Get the coordinates from the user
+	 * @return
+	 */
+	private static Point getUserPos() {
+		Point uPos = new Point();
+		System.out.println("Please Input Coordinates: ");
+		Scanner scanner = new Scanner(System.in);
+		String coordinates = scanner.nextLine();
+		scanner.close();
+		
 		int commaLoc = coordinates.indexOf(",");
 		uPos.x = Integer.parseInt(coordinates.substring(0,commaLoc));
-		uPos.y = Integer.parseInt(coordinates.substring(commaLoc + 2));
-		System.out.print(uPos);
-		for(int i : list.toArray().le){
-			int dist = (int) Point2D.distance(uPos.getX(), uPos.getY(), list.get(i).getX(), list.get(i).getY());
-			System.out.println(dist + " " + uPos + " " + list.get(i));
+		uPos.y = Integer.parseInt(coordinates.substring(commaLoc + 1));
+		
+		return uPos;
+	}
+	
+	/**
+	 * Sort the list of events by their distance from the user position
+	 * @param events
+	 * @param uPos
+	 * @return
+	 */
+	private static List<Event> sortEvents(List<Event> events, Point uPos) {
+		 Collections.sort(events, (Event e1, Event e2) 
+				-> Integer.compare(e1.getDistance(uPos), e2.getDistance(uPos)));
+		 return events;
+	}
+	
+	/**
+	 * Print the details of the first closest events to the user position
+	 * @param closestEvents
+	 * @param uPos
+	 */
+	private static void showEvents(List<Event> closestEvents, Point uPos) {
+		System.out.printf("Closest Events to (%d,%d):\n", (int)uPos.getX(), (int)uPos.getY());
+		for(int i = 0; i < closestEvents.size(); i++) {
+			System.out.println(closestEvents.get(i).getDetails() 
+					+ ", Distance " + closestEvents.get(i).getDistance(uPos));
 		}
 	}
 	
-	public static ArrayList<Point> genLoc() {
+	/**
+	 * Generate a list of unique random coordinates.
+	 * @return
+	 */
+	private static List<Point> genLoc() {
 		Set<Point> set = new HashSet<Point>();
 		Random position = new Random();
 		Point test;
 		
-		int number = 10;
-
 		for(int i = 0; i < 20; i++){
 		    test = new Point();
 		    test.x=position.nextInt(21) - 10;
@@ -42,7 +88,7 @@ public class Main {
 		    set.add(test);     
 		}
 
-		ArrayList<Point> list = new ArrayList<Point>(set);
+		List<Point> list = new ArrayList<Point>(set);
 		return(list);
 	}
 }
